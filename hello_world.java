448f76a367b6af4308114a2c4e5f5d4921e60f7d
Palpat3 <mandus.hogberg@gmail.com>
2022-11-07 16:41:07 +0100

    test2

--- hello_world.java
+++ hello_world.java
@@ -2,7 +2,9 @@ class HelloWorld{
 
 
 public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Hello world");
+    int x = 10;
+    System.out.println(x);
     
 }
 }
